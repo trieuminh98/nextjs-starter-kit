ca5da5107e3d0dc89caec55ab7a9a75f9c50dbf7
minhtintrieu-ct <minh.tintrieu@chotot.vn>
2025-07-23 14:12:24 +0700

    feat: add lint-staged and husky

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,40 @@ import type { NextRequest } from "next/server";
 
 const publicPaths = ["/login"];
 
+const imageExtensions = [
+  ".svg",
+  ".jpg",
+  ".jpeg",
+  ".png",
+  ".webp",
+  ".gif",
+  ".ico",
+];
+
+// Cache control function
+function cacheControl(request: NextRequest): NextResponse | null {
+  const { pathname } = request.nextUrl;
+  // Cache static asset
+  if (imageExtensions.some((ext) => pathname.endsWith(ext))) {
+    const response = NextResponse.next();
+    response.headers.set(
+      "Cache-Control",
+      "public, max-age=600, stale-while-revalidate=60"
+    );
+    return response;
+  }
+  // Cache special pages
+  if (pathname === "/about" || pathname.startsWith("/blog/")) {
+    const response = NextResponse.next();
+    response.headers.set(
+      "Cache-Control",
+      "public, max-age=600, stale-while-revalidate=60"
+    );
+    return response;
+  }
+  return null;
+}
+
 export async function middleware(request: NextRequest) {
   const jwtToken = request.cookies.get(KEYS.JWT_TOKEN)?.value;
   if (publicPaths.includes(request.nextUrl.pathname)) {
@@ -21,6 +55,11 @@ export async function middleware(request: NextRequest) {
     loginUrl.searchParams.set("from", request.nextUrl.pathname);
     return NextResponse.redirect(loginUrl);
   }
+
+  // Cache control
+  const cache = cacheControl(request);
+  if (cache) return cache;
+
   return NextResponse.next();
 }
 
