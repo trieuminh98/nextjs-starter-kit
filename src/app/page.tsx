128d90b272d2af7087e722ea1cf0a3d152dd8f49
minhtintrieu-ct <minh.tintrieu@chotot.vn>
2025-10-06 10:58:30 +0700

    feat(query): enhance query client with retry mechanism and customizable delay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import Link from '@/components/core/link';
 
 export default async function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center">
       Welcome to the nextjs-starter-kit
       <Link prefetch href={'/hydrated-page'}>
         Hydrated Page
+      </Link>
+      <Link prefetch href={'/experiment/custom-variant'}>
+        Custom Variant
       </Link>
     </div>
   );
--- src/helper/query-client.ts
+++ src/helper/query-client.ts
@@ -1,12 +1,37 @@
 // Since QueryClientProvider relies on useContext under the hood, we have to put 'use client' on top
 import { isServer, MutationCache, QueryClient } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 import { toast } from 'sonner';
 
 function makeQueryClient() {
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
+        // Cache time
         staleTime: 60 * 1000,
+
+        // Retry mechanism
+        retry(failureCount, error: Error) {
+          const axiosErr = error as AxiosError;
+          const status = axiosErr?.status ?? axiosErr?.response?.status;
+          if (status && ![429, 503].includes(status)) return false;
+          return failureCount < 3;
+        },
+        retryDelay(attempt, error: Error) {
+          const axiosErr = error as AxiosError;
+          const ra = axiosErr?.response?.headers?.['retry-after'];
+          const hinted = ra
+            ? isNaN(+ra)
+              ? Math.max(0, Date.parse(ra) - Date.now())
+              : +ra * 1000
+            : null;
+          if (hinted) return hinted;
+          const base = 1000;
+          const cap = 30000;
+          const exp = Math.min(cap, base * 2 ** (attempt - 1));
+          // full jitter
+          return Math.floor(Math.random() * exp);
+        },
       },
     },
     mutationCache: new MutationCache({
